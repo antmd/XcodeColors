c0d040547380f5bb7e0538b2eaca77919f2d654e
Gwynne Raskind <gwynne@darkrainfall.org>
2013-05-21 16:21:00 -0700

    Fix install script to work with paths that have spaces. Switch support to ANSI-standard style so programs that use normal escape codes show up properly. Lots of personal code style changes. 64-bit only.

--- XcodeColors.m
+++ XcodeColors.m
@@ -12,84 +12,119 @@
 #define XCODE_COLORS "XcodeColors"
 
 // How to apply color formatting to your log statements:
+// Use ANSI color escapes.
+// Supports only the "m" code.
+// Supports code 0 (reset), 1 (bold), 30-37 (fg color), 40-47 (bg color), 300 (three rgb params, fg color), 400 (three rgb params, bg color)
 // 
-// To set the foreground color:
-// Insert the ESCAPE_SEQ into your string, followed by "fg124,12,255;" where r=124, g=12, b=255.
-// 
-// To set the background color:
-// Insert the ESCAPE_SEQ into your string, followed by "bg12,24,36;" where r=12, g=24, b=36.
-// 
-// To reset the foreground color (to default value):
-// Insert the ESCAPE_SEQ into your string, followed by "fg;"
-// 
-// To reset the background color (to default value):
-// Insert the ESCAPE_SEQ into your string, followed by "bg;"
-// 
-// To reset the foreground and background color (to default values) in one operation:
-// Insert the ESCAPE_SEQ into your string, followed by ";"
-// 
-// 
+//
 // Feel free to copy the define statements below into your code.
 // <COPY ME>
 
 #define XCODE_COLORS_ESCAPE @"\033["
 
-#define XCODE_COLORS_RESET_FG  XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
-#define XCODE_COLORS_RESET_BG  XCODE_COLORS_ESCAPE @"bg;" // Clear any background color
-#define XCODE_COLORS_RESET     XCODE_COLORS_ESCAPE @";"   // Clear any foreground or background color
-
 // </COPY ME>
 
 static IMP IMP_NSTextStorage_fixAttributesInRange = nil;
-
+static bool XcodeColors_intensityStateFlag = false;
 
 
 @implementation XcodeColors_NSTextStorage
 
-void ApplyANSIColors(NSTextStorage *textStorage, NSRange textStorageRange, NSString *escapeSeq)
+static NSUInteger ParseEscapeSequence(NSString *component, NSMutableDictionary *attrs)
+{
+	// An ANSI color code has the form:
+	// \033[k;k...m
+	// There may be zero or more parameters.
+	NSRange rangeOfCodeEnd = [component rangeOfString:@"m"];
+
+	if (rangeOfCodeEnd.location == NSNotFound) // not a valid color escape code, let's not do anything with it
+		return 0;
+	
+	NSString *params = [component substringToIndex:rangeOfCodeEnd.location];
+	
+	// we only support numeric codes
+	if ([params rangeOfCharacterFromSet:[[NSCharacterSet characterSetWithCharactersInString:@"0123456789;"] invertedSet]].location != NSNotFound)
+		return 0;
+	
+	NSArray *paramList = [params componentsSeparatedByString:@";"];
+	
+	const CGFloat ansiReds[]   = { 0.0, 194.0, 37.0, 173.0, 73.0, 211.0, 51.0, 203.0, 129.0, 252.0, 49.0, 234.0, 88.0, 249.0, 20.0, 233.0 },
+				  ansiGreens[] = { 0.0, 54.0, 188.0, 173.0, 46.0, 56.0, 187.0, 204.0, 131.0, 57.0, 231.0, 236.0, 51.0, 53.0, 240.0, 235.0 },
+				  ansiBlues[]  = { 0.0, 33.0, 36.0, 39.0, 255.0, 211.0, 200.0, 205.0, 131.0, 31.0, 34.0, 35.0, 255.0, 248.0, 240.0, 235.0 };
+	
+	for (NSUInteger i = 0; i < paramList.count; ++i) { // May need to skip params, can't use for..in
+		NSUInteger param = [paramList[i] integerValue];
+		
+		switch (param) {
+			case 0: // reset
+				[attrs removeObjectsForKeys:@[NSForegroundColorAttributeName, NSBackgroundColorAttributeName]];
+				XcodeColors_intensityStateFlag = false;
+				break;
+			case 1: // bold
+				XcodeColors_intensityStateFlag = true;
+				break;
+			case 30: case 31: case 32: case 33: case 34: case 35: case 36: case 37:
+				attrs[NSForegroundColorAttributeName] = [NSColor
+					colorWithCalibratedRed:ansiReds[param - 30 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					green:ansiGreens[param - 30 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					blue:ansiBlues[param - 30 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					alpha:1.0];
+				break;
+			case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47:
+				attrs[NSBackgroundColorAttributeName] = [NSColor
+					colorWithCalibratedRed:ansiReds[param - 40 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					green:ansiGreens[param - 40 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					blue:ansiBlues[param - 40 + (!!XcodeColors_intensityStateFlag * 8)] / 255.0
+					alpha:1.0];
+				break;
+			case 300:
+				if ((i + 3) >= paramList.count)
+					return 0;
+				attrs[NSForegroundColorAttributeName] = [NSColor
+					colorWithCalibratedRed:[paramList[++i] integerValue] / 255.0
+					green:[paramList[++i] integerValue] / 255.0
+					blue:[paramList[++i] integerValue] / 255.0
+					alpha:1.0];
+				break;
+			case 400:
+				if ((i + 3) >= paramList.count)
+					return 0;
+				attrs[NSBackgroundColorAttributeName] = [NSColor
+					colorWithCalibratedRed:[paramList[++i] unsignedIntegerValue] / 255.0
+					green:[paramList[++i] unsignedIntegerValue] / 255.0
+					blue:[paramList[++i] unsignedIntegerValue] / 255.0
+					alpha:1.0];
+				break;
+			default:
+				break; // ignore unknown codes
+		}
+	}
+	return rangeOfCodeEnd.location + 1;
+}
+
+static void ApplyANSIColors(NSTextStorage *textStorage, NSRange textStorageRange, NSString *escapeSeq)
 {
 	NSRange range = [[textStorage string] rangeOfString:escapeSeq options:0 range:textStorageRange];
-	if (range.location == NSNotFound)
-	{
-		// No escape sequence(s) in the string.
+	if (range.location == NSNotFound) // No escape sequence(s) in the string.
 		return;
-	}
 	
 	// Architecture:
 	// 
 	// We're going to split the string into components separated by the given escape sequence.
 	// Then we're going to loop over the components, looking for color codes at the beginning of each component.
 	// 
-	// For example, if the input string is "Hello__fg124,12,12;World" (with __ representing the escape sequence),
-	// then our components would be: (
-	//   "Hello"
-	//   "fg124,12,12;World"
-	// )
-	// 
-	// We would find a color code (rgb 124, 12, 12) for the foreground color in the second component.
-	// At that point, the parsed foreground color goes into an attributes dictionary (attrs),
-	// and the foreground color stays in effect (for the current component & future components)
-	// until it gets cleared via a different foreground color, or a clear sequence.
-	// 
 	// The attributes are applied to the entire range of the component, and then we move onto the next component.
 	// 
 	// At the very end, we go back and apply "invisible" attributes (zero font, and clear text)
 	// to the escape and color sequences.
-	
-	
 	NSString *affectedString = [[textStorage string] substringWithRange:textStorageRange];
 	
 	// Split the string into components separated by the given escape sequence.
-	
 	NSArray *components = [affectedString componentsSeparatedByString:escapeSeq];
-	
-	NSRange componentRange = textStorageRange;
-	componentRange.length = 0;
-	
+	NSRange componentRange = { textStorageRange.location, 0 };
 	BOOL firstPass = YES;
-	
-	NSMutableArray *seqRanges = [NSMutableArray arrayWithCapacity:[components count]];
-	NSMutableDictionary *attrs = [NSMutableDictionary dictionaryWithCapacity:2];
+	NSMutableArray *seqRanges = @[].mutableCopy;
+	NSMutableDictionary *attrs = @{}.mutableCopy;
 	
 	for (NSString *component in components)
 	{
@@ -103,175 +138,30 @@ void ApplyANSIColors(NSTextStorage *textStorage, NSRange textStorageRange, NSStr
 		else
 		{
 			// componentSeqRange : Range of escape sequence within component, e.g. "fg124,12,12;"
+			NSUInteger seqLen = ParseEscapeSequence(component, attrs);
 			
-			NSColor *color = nil;
-			NSUInteger colorCodeSeqLength = 0;
-			
-			BOOL stop = NO;
-			
-			BOOL reset = !stop && (stop = [component hasPrefix:@";"]);
-			BOOL fg    = !stop && (stop = [component hasPrefix:@"fg"]);
-			BOOL bg    = !stop && (stop = [component hasPrefix:@"bg"]);
-			
-			BOOL resetFg = fg && [component hasPrefix:@"fg;"];
-			BOOL resetBg = bg && [component hasPrefix:@"bg;"];
-			
-			if (reset)
-			{
-				// Reset attributes
-				[attrs removeObjectForKey:NSForegroundColorAttributeName];
-				[attrs removeObjectForKey:NSBackgroundColorAttributeName];
-				
-				// Mark the range of the sequence (escape sequence + reset color sequence).
-				NSRange seqRange = (NSRange){
-					.location = componentRange.location - [escapeSeq length],
-					.length = 1 + [escapeSeq length],
-				};
-				[seqRanges addObject:[NSValue valueWithRange:seqRange]];
-			}
-			else if (resetFg || resetBg)
-			{
-				// Reset attributes
-				if (resetFg)
-					[attrs removeObjectForKey:NSForegroundColorAttributeName];
-				else
-					[attrs removeObjectForKey:NSBackgroundColorAttributeName];
-				
-				// Mark the range of the sequence (escape sequence + reset color sequence).
-				NSRange seqRange = (NSRange){
-					.location = componentRange.location - [escapeSeq length],
-					.length = 3 + [escapeSeq length],
-				};
-				[seqRanges addObject:[NSValue valueWithRange:seqRange]];
-			}
-			else if (fg || bg)
-			{
-				// Looking for something like this: "fg124,22,12;" or "bg17,24,210;".
-				// These are the rgb values for the foreground or background.
-				
-				NSString *str_r = nil;
-				NSString *str_g = nil;
-				NSString *str_b = nil;
-				
-				NSRange range_search = NSMakeRange(2, MIN(4, [component length] - 2));
-				NSRange range_separator;
-				NSRange range_value;
-				
-				// Search for red separator
-				range_separator = [component rangeOfString:@"," options:0 range:range_search];
-				if (range_separator.location != NSNotFound)
-				{
-					// Extract red substring
-					range_value.location = range_search.location;
-					range_value.length = range_separator.location - range_search.location;
-					
-					str_r = [component substringWithRange:range_value];
-					
-					// Update search range
-					range_search.location = range_separator.location + range_separator.length;
-					range_search.length = MIN(4, [component length] - range_search.location);
-					
-					// Search for green separator
-					range_separator = [component rangeOfString:@"," options:0 range:range_search];
-					if (range_separator.location != NSNotFound)
-					{
-						// Extract green substring
-						range_value.location = range_search.location;
-						range_value.length = range_separator.location - range_search.location;
-						
-						str_g = [component substringWithRange:range_value];
-						
-						// Update search range
-						range_search.location = range_separator.location + range_separator.length;
-						range_search.length = MIN(4, [component length] - range_search.location);
-						
-						// Search for blue separator
-						range_separator = [component rangeOfString:@";" options:0 range:range_search];
-						if (range_separator.location != NSNotFound)
-						{
-							// Extract blue substring
-							range_value.location = range_search.location;
-							range_value.length = range_separator.location - range_search.location;
-							
-							str_b = [component substringWithRange:range_value];
-							
-							// Mark the length of the entire color code sequence.
-							colorCodeSeqLength = range_separator.location + range_separator.length;
-						}
-					}
-				}
-				
-				if (str_r && str_g && str_b)
-				{
-					// Parse rgb values and create color
-					
-					int r = MAX(0, MIN(255, [str_r intValue]));
-					int g = MAX(0, MIN(255, [str_g intValue]));
-					int b = MAX(0, MIN(255, [str_b intValue]));
-					
-					color = [NSColor colorWithCalibratedRed:(r/255.0)
-					                                  green:(g/255.0)
-					                                   blue:(b/255.0)
-					                                  alpha:1.0];
-					
-					if (fg)
-					{
-						[attrs setObject:color forKey:NSForegroundColorAttributeName];
-					}
-					else
-					{
-						[attrs setObject:color forKey:NSBackgroundColorAttributeName];
-					}
-					
-					//NSString *realString = [component substringFromIndex:colorCodeSeqLength];
-					
-					// Mark the range of the entire sequence (escape sequence + color code sequence).
-					NSRange seqRange = (NSRange){
-						.location = componentRange.location - [escapeSeq length],
-						.length = colorCodeSeqLength + [escapeSeq length],
-					};
-					[seqRanges addObject:[NSValue valueWithRange:seqRange]];
-				}
-				else
-				{
-					// Wasn't able to parse a color code
-					
-					[attrs removeObjectForKey:NSForegroundColorAttributeName];
-					[attrs removeObjectForKey:NSBackgroundColorAttributeName];
-					
-					NSRange seqRange = (NSRange){
-						.location = componentRange.location - [escapeSeq length],
-						.length = [escapeSeq length],
-					};
-					[seqRanges addObject:[NSValue valueWithRange:seqRange]];
-				}
+			if (seqLen) {
+				[seqRanges addObject:[NSValue valueWithRange:(NSRange){
+					.location = componentRange.location - escapeSeq.length,
+					.length = seqLen + escapeSeq.length
+				}]];
 			}
 		}
 		
-		componentRange.length = [component length];
-		
+		componentRange.length = component.length;
 		[textStorage addAttributes:attrs range:componentRange];
-		
-		componentRange.location += componentRange.length + [escapeSeq length];
+		componentRange.location += componentRange.length + escapeSeq.length;
 		firstPass = NO;
 		
 	} // END: for (NSString *component in components)
 	
-	
 	// Now loop over all the discovered sequences, and apply "invisible" attributes to them.
+	NSDictionary *clearAttrs = @{ NSFontAttributeName: [NSFont systemFontOfSize:0.001], NSForegroundColorAttributeName: [NSColor clearColor] };
 	
-	if ([seqRanges count] > 0)
+	for (NSValue *seqRangeValue in seqRanges)
 	{
-		NSDictionary *clearAttrs =
-		    [NSDictionary dictionaryWithObjectsAndKeys:
-		        [NSFont systemFontOfSize:0.001], NSFontAttributeName,
-		                   [NSColor clearColor], NSForegroundColorAttributeName, nil];
-		
-		for (NSValue *seqRangeValue in seqRanges)
-		{
-			NSRange seqRange = [seqRangeValue rangeValue];
-			[textStorage addAttributes:clearAttrs range:seqRange];
-		}
+		NSRange seqRange = [seqRangeValue rangeValue];
+		[textStorage addAttributes:clearAttrs range:seqRange];
 	}
 }
 
@@ -288,9 +178,7 @@ void ApplyANSIColors(NSTextStorage *textStorage, NSRange textStorageRange, NSStr
 	
 	char *xcode_colors = getenv(XCODE_COLORS);
 	if (xcode_colors && (strcmp(xcode_colors, "YES") == 0))
-	{
 		ApplyANSIColors(self, aRange, XCODE_COLORS_ESCAPE);
-	}
 }
 
 @end
